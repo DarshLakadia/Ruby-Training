ca7498a80e56a4f79e31c022b9fdae69b55c422e
DarshLakadia <darsh.lakadia@bacancy.com>
2023-02-24 21:59:12 +0530

    Convert a hash into an array. Nothing more, Nothing less

---
+++ hash_to_array.rb
@@ -0,0 +1,10 @@
+=begin
+Convert a hash into an array. Nothing more, Nothing less.
+Input - {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+Output - [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+
+=end
+input = {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+hash = {}
+input.each{|k,v| hash[k.to_s] = v}
+p hash.to_a
