516a4141fdf21e90d2c5baed94a5c4b0d345ee69
DarshLakadia <126099628+DarshLakadia@users.noreply.github.com>
2023-02-28 16:55:36 +0530

    update code - sort hash by key

--- hash_to_array.rb
+++ hash_to_array.rb
@@ -4,6 +4,7 @@ Input - {name: 'Jeremy', age: 24, role: 'Software Engineer'}
 Output - [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
 
 =end
+
 input = {name: 'Jeremy', age: 24, role: 'Software Engineer'}
 hash = {}
 input.each{|k,v| hash[k.to_s] = v}
